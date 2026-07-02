3896db39d0172af6701ac9c953651450a291083b
Akinwunmi Femi <feezyakinwunmi001@gmail.com>
2026-07-02 15:04:01 +0100

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -124,7 +124,9 @@ export default function Footer() {
             &copy; {new Date().getFullYear()} LO Publications. All rights reserved.
           </p>
           <p className="mt-2">
-            Built with passion for authors • Faith-Friendly Publishing • Powered by LO Platform
+            Built with passion for authors • Faith-Friendly Publishing •  Powered by <a href="https://loplatforms.com" target="_blank" rel="noopener noreferrer" className="hover:text-red-900 transition">
+              LO Platforms
+            </a>
           </p>
         </div>
       </div>
